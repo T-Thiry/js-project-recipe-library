f7f96b503493ffaeab1e7b5f63f3282d471792b3
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-03-10 19:48:32 +0100

    making minor changes to the arrays of objects such as changing the name to title and adding servings

--- recipe.js
+++ recipe.js
@@ -3,50 +3,56 @@ console.log("script is connected")
 //Array of recipe objects
 const recipes = [
   {
-    name: 'Frutti di Mare',
+    title: 'Frutti di Mare',
     cuisine: 'Italian',
     time: 30,
+    servings: 4,
     diet: ['pescatarian', 'dairy-free'],
     image: './Images/Italian-frutti-di-mare.jpg',
     ingredients: ['400g Pasta', '500g Black mussels', '300g Shrimps (peeled)','2 Tomatoes', '3 tbs Olive oil', '1 glove Basil leaves (for garnish)']
   },
   {
-    name: 'Bucatini with Black Truffle',
+    title: 'Bucatini with Black Truffle',
     cuisine: 'Italian',
     diet: ['pescatarian', 'dairy-free'],
     time: 30,
+    servings: 4,
     image: './Images/Italian-truffle-spaghetti.jpg',
-    ingredients: ['200g Pasta', '1 Black truffle (thinly sliced)', '4 anchovy fillets (minced)', '2 Garlic (gloves)','3 tbs Olive oil','Salt (pinch)']
+    ingredients: ['400g Pasta', '1 Black truffle (thinly sliced)', '4 anchovy fillets (minced)', '2 Garlic (gloves)','3 tbs Olive oil','Salt (pinch)']
   },
   {
-    name: 'Tapas with Shrimp',
+    title: 'Tapas with Shrimp',
     cuisine: 'Spanish',
     diet: ['pescatarian', 'dairy-free'],
     time: 25,
+    servings: 4,
     image: './Images/Spanish-tapas.jpg',
     ingredients: ['1 Baguette', '2 Dozen shrimps', '1 tbs Fresh cilantro', '2 Red bell peppers (minced)', '3 tbs Olive oil', 'Salt (pinch)']
   },
   {
-    name: 'Vegetable Stew',
+    title: 'Vegetable Stew',
     cuisine: 'Spanish',
     diet: ['vegetarian', 'dairy-free'],
     time: 40,
+    servings: 6,
     image: './Images/Spanish-vegetable-stew.jpg',
     ingredients: ['6 Potatoes', '500g Flat green beans', '1/2 White Cabbage', '200 ml vegetable broth', '3 tbs Olive oil', 'Salt (pinch)']
   },
   {
-    name: 'Greek Salad',
+    title: 'Greek Salad',
     cuisine: 'Greek',
     diet: ['vegetarian'],
     time: 20,
+    servings: 2,
     image: './Images/Greek-traditional-salad.jpg',
     ingredients: ['1 Cabbage', '2 Tomatoes', ' 1/2 Cucumber', '150g Feta cheese', '1 Red onion', '300g Black olives', '3 tbs Olive oil']
   },
   {
-    name: 'Greek Salad with Pomegranate',
+    title: 'Greek Salad with Pomegranate',
     cuisine: 'Greek',
-    diet: ['vegetarian'],
+    diet: ['vegetarian', 'dairy-free'],
     time: 20,
+    servings: 2,
     image: './Images/Greek-salad.jpg',
     ingredients: ['2 Tomatoes', '1/2 Cucumber', '1 Red onion', '1/4 Red cabbage', '1 Pomegranate', '200g Corn']
   }
@@ -60,8 +66,8 @@ const listRecipes = (filteredRecipes) => {
   filteredRecipes.forEach(recipe => {
     container.innerHTML += `
       <div class="recipe-card">
-        <img src="${recipe.image}" alt="${recipe.name}">
-        <h2>${recipe.name}</h2>
+        <img src="${recipe.image}" alt="${recipe.title}">
+        <h2>${recipe.title}</h2>
         <hr>
         <h3>Cuisine: ${recipe.cuisine}</h3>
         <h3>Cooking time: ${recipe.time} minutes</h3>
@@ -104,4 +110,6 @@ document.querySelectorAll('.sort-buttons button').forEach(button => {
     }
   });
 });
+
+
 
